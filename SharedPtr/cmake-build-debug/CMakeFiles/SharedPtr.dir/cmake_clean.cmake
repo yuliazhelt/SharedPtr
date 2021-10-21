8cb5745140ab6abe691e1a47a27183944838e826
yuliazhelt <77640129+yuliazhelt@users.noreply.github.com>
2021-10-21 19:14:48 +0300

    Add files via upload

---
+++ SharedPtr/cmake-build-debug/CMakeFiles/SharedPtr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SharedPtr.dir/main.cpp.o"
+  "SharedPtr"
+  "SharedPtr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SharedPtr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
